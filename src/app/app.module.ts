174bc9f3800051610002f2ec1352fa4dbe4e5a3f
pspapleng <62070039@kmitl.ac.th>
2019-10-10 19:13:37 +0700

    add new component icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IconComponent } from './icon/icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule
