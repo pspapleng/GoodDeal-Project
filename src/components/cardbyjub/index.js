f862c2d35abc10ebc7b37fc72a2c5cbac8eab948
weerakorn <momay-weerakorn@hotmail.com>
2019-12-16 06:52:38 +0700

    add logic

--- src/components/cardbyjub/index.js
+++ src/components/cardbyjub/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -98,51 +99,240 @@ export default function SimpleCard({ deleteCallback }) {
   let handleChange = event => {
     setCurrency(event.target.value);
     if (event.target.value == "%") {
-      setValues({
-        ...values,
-        total:
-          (values.cost * (values.cost - values.discount)) / 100 +
-          values.shipping
-      });
-    } else {
-      setValues({
-        ...values,
-        total: values.cost - values.discount + values.shipping
-      });
+      setValues({...values,
+        currency: 'Percent',
+        total: (values.cost*(100-values.discount))/100 + values.shipping - values.premium}
+      );
+    }
+    else {
+      setValues({...values,
+        currency: 'Baht',
+        total: values.cost - values.discount + values.shipping - values.premium}
+      );
     }
   };
 
   let handleCost = event => {
-    setValues({
-      ...values,
-      cost: parseFloat(event.target.value),
-      total: parseFloat(event.target.value) - values.discount + values.shipping
-    });
+    if (values.currency == 'Percent') {
+      if (event.target.value == '') {
+        setValues({...values,
+          cost: 0,
+          total: (0*(100-values.discount))/100 + values.shipping - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          cost: parseFloat(event.target.value),
+          total: (parseFloat(event.target.value)*(100-values.discount))/100 + values.shipping - values.premium}
+        );
+      }
+    }
+    else {
+      if (event.target.value == '') {
+        setValues({...values,
+          cost: 0,
+          total: 0 - values.discount + values.shipping - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          cost: parseFloat(event.target.value),
+          total: parseFloat(event.target.value) - values.discount + values.shipping - values.premium}
+        );
+      }
+    }
   };
 
   let handleDiscount = event => {
-    setValues({
-      ...values,
-      discount: parseFloat(event.target.value),
-      total: values.cost - parseFloat(event.target.value) + values.shipping
-    });
+    if (values.currency == 'Percent') {
+      if (event.target.value == ''){
+        setValues({...values,
+          discount: 0,
+          total: (values.cost*(100-0))/100 + values.shipping - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          discount: parseFloat(event.target.value),
+          total: (values.cost*(100-parseFloat(event.target.value)))/100 + values.shipping - values.premium}
+        );
+      }
+    }
+    else {
+      if (event.target.value == ''){
+        setValues({...values,
+          discount: 0,
+          total: values.cost - 0 + values.shipping - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          discount: parseFloat(event.target.value),
+          total: values.cost - parseFloat(event.target.value) + values.shipping - values.premium}
+        );
+      }
+    }
   };
 
   let handleShipping = event => {
-    setValues({
-      ...values,
-      shipping: parseFloat(event.target.value),
-      total: values.cost - values.discount + parseFloat(event.target.value)
-    });
+    if (values.currency == 'Percent') {
+      if (event.target.value == '') {
+        setValues({...values,
+          shipping: 0,
+          total: (values.cost*(100-values.discount))/100 + 0 - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          shipping: parseFloat(event.target.value),
+          total: (values.cost*(100-values.discount))/100 + parseFloat(event.target.value) - values.premium}
+        );
+      }
+    }
+    else {
+      if (event.target.value == '') {
+        setValues({...values,
+          shipping: 0,
+          total: values.cost - values.discount + 0 - values.premium}
+        );
+      }
+      else {
+        setValues({...values,
+          shipping: parseFloat(event.target.value),
+          total: values.cost - values.discount + parseFloat(event.target.value) - values.premium}
+        );
+      }
+    }
   };
 
-  let [premiumArr, setPremium] = React.useState([0]);
-  let handleAdd = () => {
-    let clone = [...premiumArr];
-    let lstdx = clone.length - 1;
-    clone.push(clone[lstdx] + 1);
-    setPremium(clone);
+  let handlePremium = event => {
+    if (values.currency == 'Percent') {
+      if (event.target.value == '') {
+        setValues({...values,
+          premium: 0,
+          total: (values.cost*(100-values.discount))/100 + values.shipping - 0}
+        );
+      }
+      else {
+        setValues({...values,
+          premium: parseFloat(event.target.value),
+          total: (values.cost*(100-values.discount))/100 + values.shipping - parseFloat(event.target.value)}
+        );
+      }
+    }
+    else {
+      if (event.target.value == '') {
+        setValues({...values,
+          premium: 0,
+          total: values.cost - values.discount + values.shipping - 0}
+        );
+      }
+      else {
+        setValues({...values,
+          premium: parseFloat(event.target.value),
+          total: values.cost - values.discount + values.shipping - parseFloat(event.target.value)}
+        );
+      }
+    }
   };
+
+  // function Delete(currentEl){
+  //   currentEl.p
+  // }
+
+  // function handlePremium1(e) {
+  //   if (values.currency == 'Percent') {
+  //     if (event.target.value == '') {
+  //       setValues({...values,
+  //         premium: 0,
+  //         total: (values.cost*(100-values.discount))/100 + values.shipping - 0}
+  //       );
+  //     }
+  //     else {
+  //       setValues({...values,
+  //         premium: parseFloat(event.target.value),
+  //         total: (values.cost*(100-values.discount))/100 + values.shipping - parseFloat(event.target.value)}
+  //       );
+  //     }
+  //   }
+  //   else {
+  //     if (event.target.value == '') {
+  //       setValues({...values,
+  //         premium: 0,
+  //         total: values.cost - values.discount + values.shipping - 0}
+  //       );
+  //     }
+  //     else {
+  //       setValues({...values,
+  //         premium: parseFloat(event.target.value),
+  //         total: values.cost - values.discount + values.shipping - parseFloat(event.target.value)}
+  //       );
+  //     }
+  //   }
+  // }
+  
+
+  // let [premiumArr, setPremium] = React.useState([0]);
+  // let handleAdd = () => {
+  //   let clone = [...premiumArr];
+  //   let lstdx = clone.length - 1;
+  //   clone.push(clone[lstdx] + 1);
+  //   setPremium(clone);
+  // };
+
+  // function handleAdd()
+  // {
+  //   // var iDiv = document.createElement('div');
+  //   // iDiv.className='classes.container';
+  //   // var chl = document.createElement('TextField');
+  //   // chl.className='classes.textField';
+  //   // chl.label='Premium';
+  //   // chl.margin="normal";
+  //   // chl.variant="outlined";
+  //   // chl.onChange='handlePremium';
+  //   // iDiv.appendChild(chl);
+  //   // iDiv.className='classes.container';
+  //   // document.getElementById("5555").appendChild(iDiv);
+  //   // document.getElementById("5555").innerHTML += "<div className={classes.container}><TextField id=\"123\" className={classes.textField} label=\"Premium\" margin=\"normal\" variant=\"outlined\" onChange={handlePremium} InputProps={{ startAdornment: ( <InputAdornment position=\"start\">฿</InputAdornment> ) }} /> </div>"
+  //   var itm = document.getElementById("5555");
+  //   // var itm_2 = document.getElementById("123");
+  //   // var sss = itm_2.value;
+  //   var sss = itm.firstChild.value;
+  //   itm.firstChild.value = "";
+  //   // itm_2.value = "";
+  //   var cln = itm.cloneNode(true);
+  //   cln.firstChild.value = '';
+  //   // // cln.firstChild.value = "123";
+  //   // // var childnode = cln.childNodes[1];
+  //   // // childnode.addEventListener("onChange", handlePremium1);
+  //   document.addEventListener('change', handlePremium);
+  //   document.getElementById("555").appendChild(cln);
+  //   document.getElementById("555").innerHTML += cln.children
+  //   // document.getElementById("123").value = sss;
+  // }
+
+  // // function handleAdd(){
+  // //   var c = document.createElement('TextField')
+  // //   document.getElementById('5555').appendChild(c)
+    
+  // // }
+
+  // function handleDelete()
+  // {
+  //   var e = document.getElementById("555");
+  //   if (e.hasChildNodes()) {
+  //     e.removeChild(e.lastChild);
+  //   }
+  // }
+
+  // function addCard() {
+  //   document.getElementById('main').innerHTML = 'sdsad';
+  // }
+
+  // function deleteCard(currentEl){
+  //   currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
+  // }
+
 
   return (
     <Card className={classes.card}>
@@ -223,24 +413,30 @@ export default function SimpleCard({ deleteCallback }) {
         </div>
         <div className={classes.container}>
           <TextField
-            id="outlined-basic"
+            id="123"
             className={classes.textField}
             label="Premium"
             margin="normal"
             variant="outlined"
+            onChange={handlePremium}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">฿</InputAdornment>
               )
             }}
           />
-          <IconButton onClick={handleAdd}>
+        </div>
+        
+        <div className={classes.container} id='555'>
+        </div>
+
+          {/* <IconButton onClick={handleAdd}>
             <Add></Add>
           </IconButton>
-          <IconButton onClick={deleteCallback}>
+          <IconButton onClick={handleDelete}>
             <Delete></Delete>
-          </IconButton>
-        </div>
+          </IconButton> */}
+        
         <form className={classes.root} noValidate>
           <ValidationTextField
             className={classes.margin}
