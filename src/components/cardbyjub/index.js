ebbd888915885cff853585f5be8962432a61905d
weerakorn <momay-weerakorn@hotmail.com>
2019-12-16 07:29:32 +0700

    add logic

--- src/components/cardbyjub/index.js
+++ src/components/cardbyjub/index.js
@@ -236,104 +236,6 @@ export default function SimpleCard({ deleteCallback }) {
     }
   };
 
-  // function Delete(currentEl){
-  //   currentEl.p
-  // }
-
-  // function handlePremium1(e) {
-  //   if (values.currency == 'Percent') {
-  //     if (event.target.value == '') {
-  //       setValues({...values,
-  //         premium: 0,
-  //         total: (values.cost*(100-values.discount))/100 + values.shipping - 0}
-  //       );
-  //     }
-  //     else {
-  //       setValues({...values,
-  //         premium: parseFloat(event.target.value),
-  //         total: (values.cost*(100-values.discount))/100 + values.shipping - parseFloat(event.target.value)}
-  //       );
-  //     }
-  //   }
-  //   else {
-  //     if (event.target.value == '') {
-  //       setValues({...values,
-  //         premium: 0,
-  //         total: values.cost - values.discount + values.shipping - 0}
-  //       );
-  //     }
-  //     else {
-  //       setValues({...values,
-  //         premium: parseFloat(event.target.value),
-  //         total: values.cost - values.discount + values.shipping - parseFloat(event.target.value)}
-  //       );
-  //     }
-  //   }
-  // }
-  
-
-  // let [premiumArr, setPremium] = React.useState([0]);
-  // let handleAdd = () => {
-  //   let clone = [...premiumArr];
-  //   let lstdx = clone.length - 1;
-  //   clone.push(clone[lstdx] + 1);
-  //   setPremium(clone);
-  // };
-
-  // function handleAdd()
-  // {
-  //   // var iDiv = document.createElement('div');
-  //   // iDiv.className='classes.container';
-  //   // var chl = document.createElement('TextField');
-  //   // chl.className='classes.textField';
-  //   // chl.label='Premium';
-  //   // chl.margin="normal";
-  //   // chl.variant="outlined";
-  //   // chl.onChange='handlePremium';
-  //   // iDiv.appendChild(chl);
-  //   // iDiv.className='classes.container';
-  //   // document.getElementById("5555").appendChild(iDiv);
-  //   // document.getElementById("5555").innerHTML += "<div className={classes.container}><TextField id=\"123\" className={classes.textField} label=\"Premium\" margin=\"normal\" variant=\"outlined\" onChange={handlePremium} InputProps={{ startAdornment: ( <InputAdornment position=\"start\">฿</InputAdornment> ) }} /> </div>"
-  //   var itm = document.getElementById("5555");
-  //   // var itm_2 = document.getElementById("123");
-  //   // var sss = itm_2.value;
-  //   var sss = itm.firstChild.value;
-  //   itm.firstChild.value = "";
-  //   // itm_2.value = "";
-  //   var cln = itm.cloneNode(true);
-  //   cln.firstChild.value = '';
-  //   // // cln.firstChild.value = "123";
-  //   // // var childnode = cln.childNodes[1];
-  //   // // childnode.addEventListener("onChange", handlePremium1);
-  //   document.addEventListener('change', handlePremium);
-  //   document.getElementById("555").appendChild(cln);
-  //   document.getElementById("555").innerHTML += cln.children
-  //   // document.getElementById("123").value = sss;
-  // }
-
-  // // function handleAdd(){
-  // //   var c = document.createElement('TextField')
-  // //   document.getElementById('5555').appendChild(c)
-    
-  // // }
-
-  // function handleDelete()
-  // {
-  //   var e = document.getElementById("555");
-  //   if (e.hasChildNodes()) {
-  //     e.removeChild(e.lastChild);
-  //   }
-  // }
-
-  // function addCard() {
-  //   document.getElementById('main').innerHTML = 'sdsad';
-  // }
-
-  // function deleteCard(currentEl){
-  //   currentEl.parentNode.parentNode.removeChild(currentEl.parentNode);
-  // }
-
-
   return (
     <Card className={classes.card}>
       <CardContent className={classes.root}>
@@ -413,29 +315,20 @@ export default function SimpleCard({ deleteCallback }) {
         </div>
         <div className={classes.container}>
           <TextField
-            id="123"
+            id="outlined-basic"
             className={classes.textField}
             label="Premium"
             margin="normal"
             variant="outlined"
-            onChange={handlePremium}
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">฿</InputAdornment>
               )
             }}
+            onChange={handlePremium}
           />
         </div>
-        
-        <div className={classes.container} id='555'>
-        </div>
 
-          {/* <IconButton onClick={handleAdd}>
-            <Add></Add>
-          </IconButton>
-          <IconButton onClick={handleDelete}>
-            <Delete></Delete>
-          </IconButton> */}
         
         <form className={classes.root} noValidate>
           <ValidationTextField
